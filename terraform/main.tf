7b05507b929a0b6cc17e8444912e25dc32ffe9c6
bonaokolo <bonaokolo@gmail.com>
2025-06-08 00:22:42 +0000

    helloooo

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,8 +1,22 @@
+variable "region" {
+  default = "us-east-1"
+}
+
+variable "key_name" {
+  type = string
+}
+
+variable "existing_instance_id" {
+  type    = string
+  default = ""
+}
+
 provider "aws" {
   region = var.region
 }
 
 resource "aws_instance" "web" {
+  count         = var.existing_instance_id == "" ? 1 : 0
   ami           = "ami-0c02fb55956c7d316" # Ubuntu 22.04 in us-east-1
   instance_type = "t2.micro"
   key_name      = var.key_name
@@ -22,10 +36,15 @@ resource "aws_instance" "web" {
   }
 }
 
+data "aws_instance" "existing" {
+  count       = var.existing_instance_id != "" ? 1 : 0
+  instance_id = var.existing_instance_id
+}
+
 output "instance_id" {
-  value = aws_instance.web.id
+  value = var.existing_instance_id != "" ? data.aws_instance.existing[0].id : aws_instance.web[0].id
 }
 
 output "instance_ip" {
-  value = aws_instance.web.public_ip
+  value = var.existing_instance_id != "" ? data.aws_instance.existing[0].public_ip : aws_instance.web[0].public_ip
 }
